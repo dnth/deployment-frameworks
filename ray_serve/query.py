90a42e6806545850d251ed56121b294e16e69be4
dnth <dickson.neoh@gmail.com>
2024-11-19 14:57:06 +0800

    use pathlib

--- ray_serve/query.py
+++ ray_serve/query.py
@@ -3,11 +3,10 @@ from pathlib import Path
 import requests
 
 # Replace with your image file path
-image_path = "/home/dnth/Desktop/xinfer/assets/demo/0a6ee446579d2885.jpg"
+image_path = Path("/home/dnth/Desktop/xinfer/assets/demo/0a6ee446579d2885.jpg")
 
 # Read the image file as binary data
-with open(image_path, "rb") as f:
-    image_bytes = f.read()
+image_bytes = image_path.read_bytes()
 
 # Make POST request to the service
 # By default, Ray Serve runs on port 8000
